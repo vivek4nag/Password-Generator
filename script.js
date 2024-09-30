11d56145fafacfce1cdcd7ab2ed09a2cacec8f42
viek4nag <vivek4nag@gmail.com>
2024-10-01 00:23:29 +0530

    html css done & JS added

--- script.js
+++ script.js
@@ -1,27 +1,3 @@
-const passwordBox = document.getElementById("password")
-const length = 14;
-
-const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-const lowerCase = "abcdefghijklmnopqrstuvwxyz";
-const number = "0123456789";
-const symbol = "!@#$%^&*()<>?/{}[]~+-=|";
-
-const allChars = upperCase + lowerCase + number + symbol
-
-function createPassword () {
-    let password = "";
-    //below 4 line just to make sure ki saare character 1-1 krke aajaye
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += number[Math.floor(Math.random() * number.length)];
-    password += symbol[Math.floor(Math.random() * symbol.length)];
-
-    while (length > password.length){
-        password += allChars[Math.floor(Math.random() * allChars.length)];
-    }
-
-    passwordBox.value = password;
-}
 
 function copypassword(){
     passwordBox.select(); // highlights (selects) the entire content (the generated password) inside the passwordBox input field.
